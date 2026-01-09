4cce0193811a0e937aff7d180a5819e157402b38
mustafabacchus <mbac246@gmail.com>
2026-01-09 16:49:31 -0500

    refactored files

--- tests/navigation.spec.js
+++ tests/navigation.spec.js
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 import { goToLandingPage, Login } from './helper';
 
 
-test.describe('Navigation Banner', () => {
+test.describe('Main Navigation Banner', () => {
   let page;
   test.beforeEach(async ({ browser }) => {
     page = await browser.newPage();
